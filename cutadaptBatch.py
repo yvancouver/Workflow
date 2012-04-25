a885f0f9f22d870031f9193ee5b103e24936f661
yvan <yvan.strahm@gmail.com>
2012-04-25 22:53:00 +0200

    Start a argparse version of cutadaptBatch

--- cutadaptBatch.py
+++ cutadaptBatch.py
@@ -6,16 +6,38 @@
 ## the script should traverse a directory found the read and call adapt with the correct option
 ## cutadapt -m 20 -a adapter -o fastq.clipped.gz original.fastq.gz
 ## args should be ['cutadapt', '-m', '20', '-a', 'AGATCGGAAGAGCACACGTCTGAACTCCAGTCAC', '-o', '/Users/yvans/Home/Analysis/Cardio_analysis_01.17.2012/Project_Diag-Cardiomyopathy-2011-12-09/Sample_KA-001/KA-001_ATCACG_L001_R1_001.clipped_m20.fastq.gz', '/Users/yvans/Home/Analysis/Cardio_analysis_01.17.2012/Project_Diag-Cardiomyopathy-2011-12-09/Sample_KA-001/KA-001_ATCACG_L001_R1_001.fastq.gz']
+## have a look here http://argparse.googlecode.com/svn/trunk/doc/argparse-vs-optparse.html
+##                  http://docs.python.org/library/argparse.html#module-argparse
+#
 
 import re
 import sys
 import os
-#import subprocess
+import argparse
+import textwrap
+
 from subprocess import Popen, PIPE, STDOUT
 import shlex
+#for arg in sys.argv:
+#    print arg , " is a ", type(arg)
 
-for arg in sys.argv:
-    print arg , " is a ", type(arg)
+parser = argparse.ArgumentParser(prog='ScutadaptBatch',
+                        formatter_class=argparse.RawDescriptionHelpFormatter,
+                        description=textwrap.dedent('''\
+                            cutadaptBatch is use to clipped a gunziped fastq file with, for now, some Illumina adapters
+                            This was made in order to clean some Halo produced data.
+                            Author: Yvan Strahm (yvan.strahm@gmail.com)
+                            License: GPL 3.0 (http://www.gnu.org/licenses/gpl-3.0.txt)
+                            '''))
+parser.add_argument('-m', help='match region length', required=True)
+parser.add_argument('-d', help='fastq.gz containing dir', required=True)
+parser.add_argument('-r1', help='Read1 adapter',required=False)
+parser.add_argument('-r2', help='Read2 adapter',required=False)
+values = parser.parse_args()
+print values.m
+print values.d
+
+sys.exit("on s'arretes ici pour l'instant")
 
 r1_adapter = "AGATCGGAAGAGCACACGTCTGAACTCCAGTCAC"
 r2_adapter = "AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGTAGATCTCGGTGGTCGCCGTATCATT"
