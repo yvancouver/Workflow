c27058cbce5f2108960a270b63f6ba2d1ad76c8d
Yvan <yvan.strahm@gmail.com>
2012-04-26 15:05:17 +0200

    q

--- cutadaptBatch_OLD.py
+++ cutadaptBatch_OLD.py
@@ -13,8 +13,6 @@ import os
 
 from subprocess import Popen, PIPE, STDOUT
 import shlex
-#for arg in sys.argv:
-#    print arg , " is a ", type(arg)
 
 r1_adapter = "AGATCGGAAGAGCACACGTCTGAACTCCAGTCAC"
 r2_adapter = "AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGTAGATCTCGGTGGTCGCCGTATCATT"
@@ -23,15 +21,8 @@ m = sys.argv[2]
 
 
 def cutadaptMe(file,adapter,m):
-    #from subprocess import Popen, PIPE, STDOUT
-    print file
-    print adapter
-    print m
     cmd = "cutadapt -m "+ m + " -a "+ adapter +" -o "+ file[:-8]+"clipped_m"+m+".fastq.gz" + "  "+file
-    print "CMD\t", cmd
     args = shlex.split(cmd)
-    print "ARGS\t",args
-    print "\n\n"
     job = Popen(args, stdout=PIPE, stdin=PIPE, stderr=STDOUT)
     output = job.communicate()
     results=file[:-8]+".report"
@@ -39,20 +30,14 @@ def cutadaptMe(file,adapter,m):
     result_handle.write(output[0])
 
 for dirname, dirnames, filenames in os.walk(root, topdown=True):
-    for dir in dirnames:
-        #print "DIRNAME\t",dirname
-        #print "DIR\t",dir
-        #print "FILENAMES\t",filenames
-        for file in filenames:
-            if (re.search("R1_001.fastq.gz", file)):
-                #print dirname,dir,file
-                adapter = r1_adapter
+    for file in filenames:
+        if (re.search("R1_001.fastq.gz", file)):
+            adapter = r1_adapter
                 fileToBeClipped=str(os.path.join(dirname,file))
-                print "FILETOBECLIPPED1 ",fileToBeClipped
-                cutadaptMe(fileToBeClipped,adapter,m)
-            elif (re.search("R2_001.fastq.gz", file)):
-                #print dirname,dir,file
-                adapter  = r2_adapter
-                fileToBeClipped=str(os.path.join(dirname,file))
-                print "FILETOBECLIPPED2 ",fileToBeClipped
-                cutadaptMe(fileToBeClipped,adapter,m)
+            print "FILETOBECLIPPED1 ",fileToBeClipped
+            cutadaptMe(fileToBeClipped,adapter,m)
+        elif (re.search("R2_001.fastq.gz", file)):
+            adapter  = r2_adapter
+            fileToBeClipped=str(os.path.join(dirname,file))
+            print "FILETOBECLIPPED2 ",fileToBeClipped
+            cutadaptMe(fileToBeClipped,adapter,m)
