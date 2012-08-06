e47b77b558db13494df56d833661bc147497d444
yvan <yvan.strahm@gmail.com>
2012-08-06 14:23:26 +0200

    a clean one

--- bedtools_coverage.py
+++ bedtools_coverage.py
@@ -1,8 +1,8 @@
 import sys
 import pybedtools
 
-#a = pybedtools.BedTool('/Volumes/ToveExFat/120615/040_KA005/020_refineAlignment/030_BQRecalGATK/all.realigned.markDup.baseQreCali.bam')
-a = pybedtools.BedTool('/Volumes/ToveExFat/120615/040_KA005/020_refineAlignment/030_BQRecalGATK/all.realigned.markDup.baseQreCali1000b.bam')
+a = pybedtools.BedTool('/Volumes/ToveExFat/120615/040_KA005/020_refineAlignment/030_BQRecalGATK/all.realigned.markDup.baseQreCali.bam')
+#a = pybedtools.BedTool('/Volumes/ToveExFat/120615/040_KA005/020_refineAlignment/030_BQRecalGATK/all.realigned.markDup.baseQreCali1000b.bam')
 #print type(a)
 #print "Is a ",a.fn," a bam file ? ", a._isbam
 
@@ -11,20 +11,8 @@ b = pybedtools.BedTool(sys.argv[1])
 #print "Is b ",b.fn," a bam file ? ",b._isbam
 #print"command c = a.coverage(b,d=True)"
 
-#c = a.coverage(b,d=True)
-c = pybedtools.BedTool('/Volumes/ToveExFat/120615/040_KA005/CoverageYvan/040_KA005_uscsRefSeq33genes_b37_zeroTest.bed')
-'''for i in c[0:len(c)]:
-    if int(i[7]) == 0:
-        print str(i).strip()
-'''
-#chrom = ""
-#start = ""
-#stop = ""
-feature = "None"
-#score = ""
-#strand = ""
-posinfeat = ""
-coverage = ""
+c = a.coverage(b,d=True)
+#c = pybedtools.BedTool('/Volumes/ToveExFat/120615/040_KA005/CoverageYvan/040_KA005_uscsRefSeq33genes_b37_zeroTest.bed')
 
 '''''
 1    1    50    NR_047544_exon_0_0_chr1_156052369_f    0    +    1    0
@@ -40,19 +28,12 @@ coverage = ""
 7    1000    2000    NM_001166220_exon_5_0_chr7_35293105_r    0    -    459    0
 7    1000    2000    NM_001166220_exon_5_0_chr7_35293105_r    0    -    460    0
 '''
-def CollectCov(coverageResult,cov,feature):
-    i = 0
+def CollectCov(coverageResult,cov):
     chrom = ""
     start = 0
     stop = 0
-    #score = ""
-    #strand = ""
-    #lengthFeature = 0
-    #print "chrom\tstart\tstop\tfeature\t\t\t\t\tscore\tstrand"
-    for entry in  coverageResult[0:len(coverageResult)]:
-        i += 1
-        #print "at line ",str(i),"entry is ",entry
-        if int(entry[7]) <= int(cov):
+    feature = "None"
+    
 ## Get the chrom, start and stop
 ## From the example above we can see that chrom, start, stop, feature,score, and strand stau the same only posinfeat change
 ## One should return an line with summarizing the "strech" of the feature satisfying the wanted coverage.
@@ -60,56 +41,34 @@ def CollectCov(coverageResult,cov,feature):
 ## 1    1    30    NR_047544_exon_0_0_chr1_156052369_f    0    +
 ## 7    1446 1460    NM_001077653_exon_7_0_chr7_35293105_r    0    -
 ## 7    1446 1460    NM_001166220_exon_5_0_chr7_35293105_r    0    -
+    for entry in  coverageResult[0:len(coverageResult)]:
+        if int(entry[7]) <= int(cov):
 
             if feature != entry.name:
 ## new feature so have to get his name and the start position (column 6)
                 if feature == "None":
-                    #print "I saw None"
                     if entry.start == 1:
                         start = entry.start+int(entry[6])-2
                     else:    
                         start = entry.start+int(entry[6])-1
-                    #start = entry.start
-                    #current_feature = entry.name
                     feature = entry.name
-                    #print "Got this start ", start
                 if feature != "None" and feature != entry.name:
-                    #print"I have seen a new feature name ", entry.name, " the previous was named " , feature
-                    #print "\t and it was "
-                    print "\tchrom",chrom,"\tstart",str(start),"\tstop",str(stop),"\tfeature",feature,"\tscore",entry.score,"\tstrand",entry.strand
+                    print chrom,"\t",str(start),"\t",str(stop),"\t",feature,"\t",entry.score,"\t",entry.strand
                     if entry.start == 1:
                         start = entry.start+int(entry[6])-2
                     else:    
                         start = entry.start+int(entry[6])-1
-                        #start = entry.start+int(entry[6])-1
-                    #start = entry.start
-                    #current_feature = entry.name
                     feature = entry.name
-                    #print "Got this start ", start
-                #print "entry_start",entry.start
-                #print "entry_6", entry[6]
-                #
-                #print chrom,"\t",str(start-1),"\t",str(stop),"\t",current_feature,"\t",entry.score,"\t",entry.strand
-                #print "current :",current_feature
-                #print chrom,"\t",str(start-1),"\t",str(stop),"\t",current_feature,"\t",entry.score,"\t",entry.strand
             else:
-                #print chrom,"\t",str(start-1),"\t",str(stop),"\t",current_feature,"\t",entry.score,"\t",entry.strand
-                #lengthFeature = int(entry[6])
-                #print "New length " , lengthFeature
                 chrom = entry[0]
-                #print "New Chrom" , chrom
                 if entry.start == 1:
                     stop = entry.start + int(entry[6])-1
                 else:
                     stop = entry.start + int(entry[6])
-                #print "New stop ", stop
-                #score = entry.score
-                #strand = entry.strand
-                #current_feature = entry.name
-    
+                
     last_entry = coverageResult[len(coverageResult)-1]
-    print "\tchrom",last_entry.chrom,"\tstart",str(start),"\tstop",str(stop),"\tfeature",last_entry.name,"\tscore",last_entry.score,"\tstrand",last_entry.strand
-CollectCov(c,0,feature)
+    print last_entry.chrom,"\t",str(start),"\t",str(stop),"\t",last_entry.name,"\t",last_entry.score,"\t",last_entry.strand
+CollectCov(c,0)
 
 
 
