df96f4e74566bbdeb0aeba5bc89fed0ca41675ff
Yvan <yvan.strahm@gmail.com>
2012-09-20 10:30:01 +0200

    remove the export command

--- WorkFlowGeneralDevelopment.sh
+++ WorkFlowGeneralDevelopment.sh
@@ -2,6 +2,14 @@
 # Should the different DB and soft should be declared in this script
 ##
 
+# Cutadapt
+# Version ?
+CUT=/volumm
+mCUT=32
+adapter1=AGATCGGAAGAGCACACGTCTGAACTCCAGTCAC
+adapter2=AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGTAGATCTCGGTGGTCGCCGTATCATT
+
+
 # BWA
 # Version: 0.5.9-r26-dev
 export BWA=/Volumes/data.odin/software/mac/bwa/bwa-0.5.10
@@ -19,6 +27,21 @@ export OMNI=/Volumes/data.odin/common/GATK_resource_bundle/1.2/b37/1000G_omni2.5
 export Mills_1000G_gold=/Volumes/data.odin/common/GATK_resource_bundle/1.2/b37/Mills_and_1000G_gold_standard.indels.b37.sites.vcf
 export ANNOVAR=/Volumes/data.odin/software/mac/annovar/annovar_2012May25/
 
+DB=/Volumes/data.odin/common/GATK_resource_bundle/1.2/b37/bwa_v5.10/human_g1k_v37_decoy.fasta
+# What version should we use??
+GATK=/Users/yvans/Home/bin/GenomeAnalysisTK-1.4-37-g0b29d54/
+#
+PICARD=/Volumes/data.odin/software/mac/picard/picard-tools-1.62/picard-tools-1.6
+DBSNP=/Volumes/data.odin/common/GATK_resource_bundle/1.2/b37/dbsnp_132.b37.vcf"
+HAPMAP=/Users/yvans/Home/bin/GATK_resource_bundle_from_Ying_17_01_2012/1.2/b37/hapmap_3.3.b37.sites.vcf
+OMNI=/Users/yvans//Home/bin/GATK_resource_bundle_from_Ying_17_01_2012/1.2/b37/1000G_omni2.5.b37.sites.vcf
+Mills_Gold_1000=/Volumes/data.odin/common/GATK_resource_bundle/1.2/b37/
+
+# hsMetrics
+BAIT=
+TARGET=
+
+RG="@RG\tID:1\tPL:ILLUMINA\tSM:"$PREFIX"_paired_a_m32"
 # LOG
 export LOG="$PWD/$0_`date '+%F_%T'`.log"
 export TimeUsed=timeUsed.txt
@@ -45,8 +68,14 @@ RG:\t\t$RG
 
 "> $LOG
 
-export READS1=
-export READS2=
+READS1=
+READS2=
+
+
+date
+
+# test if the two files are different. Other test should follow, like are they really paired end file 
+# XXXXX_R1_XXX.fastq.gz and XXXX_R2_XXX.fastq.gz
 
 if [ $READS1 != $READS2 ] ; then  echo NOT  ; fi
 
@@ -56,19 +85,20 @@ echo $READS2
 echo
 
 echo "cutadapt r1"
-time cutadapt -m 32 -a AGATCGGAAGAGCACACGTCTGAACTCCAGTCAC \
+time $CUT -m $mCUT -a $adapter1 \
 $READS1 > ${READS1%.fastq.gz}_Cutadapt_a_m32.fastq \
 2> ${READS1%.fastq.gz}_cutadapt_R1_a_m32.log
 
 echo
 echo "cutadapt r2"
-time cutadapt -m 32 -a AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGTAGATCTCGGTGGTCGCCGTATCATT \
+time $CUT -m $mCUT  -a $adapter2 \
 $READS2 > ${READS2%.fastq.gz}_Cutadapt_a_m32.fastq \
 2> ${READS2%.fastq.gz}_cutadapt_R2_a_m32.log
 
 #dont forget to change the line number
 
 export PREFIX=${READS1%_L00X_R1_001.fastq.gz}
+
 echo "$PREFIX"
 
 echo
@@ -87,22 +117,19 @@ gzip ${READS2%.fastq.gz}_Cutadapt_a_m32.fastq
 
 echo
 echo "mapping r1"
-time /Users/yvans/Home/bin/bwa-0.5.9/bwa aln -t 3 \
-/Users/yvans/Home/bin/GATK_resource_bundle_from_Ying_17_01_2012/1.2/b37/bwa_v5.10/human_g1k_v37_decoy.fasta \
+time $BWA aln -t 3 $DB \
 "$PREFIX"_paired_r1.fq >"$PREFIX"_paired_r1.sai \
 2> "$PREFIX"_paired_r1.sai.log
 
 echo
 echo "mapping r2"
-time /Users/yvans/Home/bin/bwa-0.5.9/bwa aln -t 3 \
-/Users/yvans/Home/bin/GATK_resource_bundle_from_Ying_17_01_2012/1.2/b37/bwa_v5.10/human_g1k_v37_decoy.fasta \
+time $BWA aln -t 3 $DB \
 "$PREFIX"_paired_r2.fq > "$PREFIX"_paired_r2.sai \
 2> "$PREFIX"_paired_r2.sai.log
 
 echo
 echo "pairing"
-time /Users/yvans/Home/bin/bwa-0.5.9/bwa sampe  -r "@RG\tID:1\tPL:ILLUMINA\tSM:"$PREFIX"_paired_a_m32" \
-/Users/yvans/Home/bin/GATK_resource_bundle_from_Ying_17_01_2012/1.2/b37/bwa_v5.10/human_g1k_v37_decoy.fasta \
+time $BWA sampe -r $RG $DB \
 "$PREFIX"_paired_r1.sai "$PREFIX"_paired_r2.sai "$PREFIX"_paired_r1.fq "$PREFIX"_paired_r2.fq > "$PREFIX"_paired.sam 2>sampe.log
 
 gzip "$PREFIX"_paired_r1.fq
@@ -110,14 +137,14 @@ gzip "$PREFIX"_paired_r2.fq
 
 echo
 echo "Picard sort"
-time java -Xmx8g -jar /Users/yvans/Home/bin/picard-tools-1.62/picard-tools-1.62/SortSam.jar \
+time java -Xmx8g -jar $PICARD/SortSam.jar \
 I="$PREFIX"_paired.sam \
 O="$PREFIX"_paired.bam \
 SO=coordinate 2>errSortSam
 
 echo "Check for the bam file validity"
 
-java -Xmx8g -jar /Users/yvans/Home/bin/picard-tools-1.62/picard-tools-1.62/ValidateSamFile.jar \
+java -Xmx8g -jar $PICARD/ValidateSamFile.jar \
 INPUT="$PREFIX"_paired.bam \
 MODE=SUMMARY \
 MAX_OUTPUT=null
@@ -125,7 +152,7 @@ MAX_OUTPUT=null
 
 echo
 echo "Picard Fixmate"
-time java -Xmx8g -jar /Users/yvans/Home/bin/picard-tools-1.62/picard-tools-1.62/FixMateInformation.jar \
+time java -Xmx8g -jar $PICARD/FixMateInformation.jar \
 I="$PREFIX"_paired.bam \
 O="$PREFIX"_pairedFixed.bam \
 2>errFixMate
@@ -137,14 +164,14 @@ rm -f "$PREFIX"_paired_r2.fq
 
 echo
 echo "Picard indexing"
-time java -Xmx8g -jar /Users/yvans/Home/bin/picard-tools-1.62/picard-tools-1.62/BuildBamIndex.jar \
+time java -Xmx8g -jar $PICARD/BuildBamIndex.jar \
 INPUT="$PREFIX"_pairedFixed.bam \
 VALIDATION_STRINGENCY=SILENT \
 2>errBuildBamIndex
 
 echo
 echo "flagstat"
-time /Users/yvans/Home/bin/samtools-0.1.18/samtools flagstat "$PREFIX"_pairedFixed.bam > flagstats.txt
+time $SAMTOOLS flagstat "$PREFIX"_pairedFixed.bam > flagstats.txt
 
 echo
 echo "mapped.txt"
@@ -159,7 +186,7 @@ time /Users/yvans/Home/bin/samtools-0.1.18/samtools view -X "$PREFIX"_pairedFixe
 
 echo
 echo "InsertMetrics"
-time java -Xmx8g -jar /Users/yvans/Home/bin/picard-tools-1.62/picard-tools-1.62/CollectInsertSizeMetrics.jar \
+time java -Xmx8g -jar $PICARD/CollectInsertSizeMetrics.jar \
 INPUT="$PREFIX"_pairedFixed.bam \
 OUTPUT=insertSizeMetrics.txt \
 HISTOGRAM_FILE=insertSizeHistogram.pdf \
@@ -167,7 +194,7 @@ HISTOGRAM_FILE=insertSizeHistogram.pdf \
 
 echo
 echo "mark duplicates"
-time java -Xmx8g -jar /Users/yvans/Home/bin/picard-tools-1.62/picard-tools-1.62/MarkDuplicates.jar \
+time java -Xmx8g -jar $PICARD/MarkDuplicates.jar \
 INPUT="$PREFIX"_pairedFixed.bam \
 OUTPUT="$PREFIX"_pairedFix.dup.bam \
 METRICS_FILE=duplicates_metrics.txt \
@@ -178,33 +205,34 @@ rm -f "$PREFIX"_pairedFix.dup.bam
 
 echo
 echo "Realign"
-time java -Xmx8g -jar /Users/yvans/Home/bin/GenomeAnalysisTK-1.4-37-g0b29d54/GenomeAnalysisTK.jar \
+time java -Xmx8g -jar $GATK/GenomeAnalysisTK.jar \
 -T RealignerTargetCreator \
--R /Users/yvans/Home/bin/GATK_resource_bundle_from_Ying_17_01_2012/1.2/b37/bwa_v5.10/human_g1k_v37_decoy.fasta \
+-R $DB \
 -o "$PREFIX"_paired.intervals \
 -I "$PREFIX"_pairedFixed.bam \
---known /Users/yvans/Home/bin/GATK_resource_bundle_from_Ying_17_01_2012/1.2/b37/dbsnp_132.b37.vcf > realignerTargetCreatorInfo.txt \
-2>errRealignerTargetCreator 
+--known $DBSNP \
+> realignerTargetCreatorInfo.txt 2>errRealignerTargetCreator 
 
 echo
 echo "Indel realigner"
-time java -Xmx8g -jar /Users/yvans/Home/bin/GenomeAnalysisTK-1.4-37-g0b29d54/GenomeAnalysisTK.jar \
+time java -Xmx8g -jar $GATK/GenomeAnalysisTK.jar \
 -T IndelRealigner \
 -maxReads 1000000 \
 -I "$PREFIX"_pairedFixed.bam \
--R /Users/yvans/Home/bin/GATK_resource_bundle_from_Ying_17_01_2012/1.2/b37/bwa_v5.10/human_g1k_v37_decoy.fasta \
+-R $DB \
 -targetIntervals "$PREFIX"_paired.intervals \
 -o "$PREFIX"_paired.realigned.bam \
 -compress 0  > indelRealignerInfo.txt 2>errIndelRealigner
+
 
 rm -f "$PREFIX"_pairedFixed.bam
 
 echo
 echo "count variant before"
-time java -Xmx8g -jar /Users/yvans/Home/bin/GenomeAnalysisTK-1.4-37-g0b29d54/GenomeAnalysisTK.jar \
+time java -Xmx8g -jar $GATK/GenomeAnalysisTK.jar \
 -l INFO \
--R /Users/yvans/Home/bin/GATK_resource_bundle_from_Ying_17_01_2012/1.2/b37/bwa_v5.10/human_g1k_v37_decoy.fasta \
--knownSites /Users/yvans/Home/bin/GATK_resource_bundle_from_Ying_17_01_2012/1.2/b37/dbsnp_132.b37.vcf \
+-R $DB \
+-knownSites $DNSNP \
 -I "$PREFIX"_paired.realigned.bam \
 -T CountCovariates -cov ReadGroupCovariate \
 -cov QualityScoreCovariate \
@@ -215,22 +243,23 @@ time java -Xmx8g -jar /Users/yvans/Home/bin/GenomeAnalysisTK-1.4-37-g0b29d54/Gen
 
 echo
 echo " recalibration"
-time java -Xmx8g -jar /Users/yvans/Home/bin/GenomeAnalysisTK-1.4-37-g0b29d54/GenomeAnalysisTK.jar \
+time java -Xmx8g -jar $GATK/GenomeAnalysisTK.jar \
 -l INFO \
--R /Users/yvans/Home/bin/GATK_resource_bundle_from_Ying_17_01_2012/1.2/b37/bwa_v5.10/human_g1k_v37_decoy.fasta \
+-R $DB \
 -I "$PREFIX"_paired.realigned.bam \
 -T TableRecalibration \
 --out "$PREFIX"_paired.baseQreCali.bam \
--recalFile table.recal_data.pre.csv -OQ -pQ 5 > tableRecalibrationInfo.txt 2>errTableRecalibration
+-recalFile table.recal_data.pre.csv -OQ -pQ 5 \
+> tableRecalibrationInfo.txt 2>errTableRecalibration
 
 rm -f "$PREFIX"_paired.realigned.bam
 
 echo
 echo "count variant after"
-time java -Xmx8g -jar /Users/yvans/Home/bin/GenomeAnalysisTK-1.4-37-g0b29d54/GenomeAnalysisTK.jar \
+time java -Xmx8g -jar $GATK/GenomeAnalysisTK.jar \
 -l INFO \
--R /Users/yvans/Home/bin/GATK_resource_bundle_from_Ying_17_01_2012/1.2/b37/bwa_v5.10/human_g1k_v37_decoy.fasta \
--knownSites /Users/yvans/Home/bin/GATK_resource_bundle_from_Ying_17_01_2012/1.2/b37/dbsnp_132.b37.vcf \
+-R $DB \
+-knownSites $DBSNP \
 -I "$PREFIX"_paired.baseQreCali.bam \
 -T CountCovariates \
 -cov ReadGroupCovariate \
@@ -242,11 +271,12 @@ time java -Xmx8g -jar /Users/yvans/Home/bin/GenomeAnalysisTK-1.4-37-g0b29d54/Gen
 
 echo "Check for the final bam file validity"
 
-java -Xmx8g -jar /Users/yvans/Home/bin/picard-tools-1.62/picard-tools-1.62/ValidateSamFile.jar \
+java -Xmx8g -jar $PICARD/ValidateSamFile.jar \
 INPUT="$PREFIX"_paired.baseQreCali.bam \
 MODE=SUMMARY \
 MAX_OUTPUT=null
 >final_bam_validity.txt
+
 echo
 echo "create pre and post folder"
 mkdir pre_recal
@@ -256,21 +286,21 @@ echo
 echo "evaluation of recalibration"
 echo
 echo "pre"
-time java -Xmx8g -jar /Users/yvans/Home/bin/GenomeAnalysisTK-1.4-37-g0b29d54/AnalyzeCovariates.jar \
+time java -Xmx8g -jar $GATK/AnalyzeCovariates.jar \
 -recalFile table.recal_data.pre.csv \
 -outputDir pre_recal/ \
 -ignoreQ 5 2>errPlotpre > plotPreInfo.txt
 
 echo
 echo "post"
-time java -Xmx8g -jar /Users/yvans/Home/bin/GenomeAnalysisTK-1.4-37-g0b29d54/AnalyzeCovariates.jar \
+time java -Xmx8g -jar $GATK/AnalyzeCovariates.jar \
 -recalFile table.recal_data.post.csv \
 -outputDir post_recal/ \
 -ignoreQ 5 2>errPlotpost > plotPostInfo.txt
 
 echo
 echo "hsMetrics"
-time java -Xmx8g -jar /Users/yvans/Home/bin/picard-tools-1.62/picard-tools-1.62/CalculateHsMetrics.jar \
+time java -Xmx8g -jar $PICARD/CalculateHsMetrics.jar \
 INPUT="$PREFIX"_paired.baseQreCali.bam \
 OUTPUT=hsMetrics.txt \
 TARGET_INTERVALS=/Users/yvans/Home/Analysis/Cardio_analysis_01.17.2012/SingleSample/010_alignment/CardioAllRefSeqExons_ucsc_corr20.interval_list  \
@@ -283,8 +313,8 @@ echo "reformat hsmetrics"
 
 echo
 echo "unified genotyper SNPs"
-time java -Xmx8g -jar /Users/yvans/Home/bin/GenomeAnalysisTK-1.4-37-g0b29d54/GenomeAnalysisTK.jar \
--R /Users/yvans/Home/bin/GATK_resource_bundle_from_Ying_17_01_2012/1.2/b37/bwa_v5.10/human_g1k_v37_decoy.fasta \
+time java -Xmx8g -jar $GATK/GenomeAnalysisTK.jar \
+-R $DB \
 -T UnifiedGenotyper \
 -glm SNP \
 -A AlleleBalance \
@@ -299,8 +329,8 @@ time java -Xmx8g -jar /Users/yvans/Home/bin/GenomeAnalysisTK-1.4-37-g0b29d54/Gen
 
 echo
 echo "unified genotyper INDELs"
-time java -Xmx8g -jar /Users/yvans/Home/bin/GenomeAnalysisTK-1.4-37-g0b29d54/GenomeAnalysisTK.jar \
--R /Users/yvans/Home/bin/GATK_resource_bundle_from_Ying_17_01_2012/1.2/b37/bwa_v5.10/human_g1k_v37_decoy.fasta \
+time java -Xmx8g -jar $GATK/GenomeAnalysisTK.jar \
+-R $DB \
 -T UnifiedGenotyper \
 -glm INDEL \
 -A AlleleBalance \
@@ -314,8 +344,8 @@ time java -Xmx8g -jar /Users/yvans/Home/bin/GenomeAnalysisTK-1.4-37-g0b29d54/Gen
 
 echo
 echo "hard filtration SNPs"
-time java -Xmx8g -jar /Users/yvans/Home/bin/GenomeAnalysisTK-1.4-37-g0b29d54/GenomeAnalysisTK.jar \
--R /Users/yvans/Home/bin/GATK_resource_bundle_from_Ying_17_01_2012/1.2/b37/bwa_v5.10/human_g1k_v37_decoy.fasta \
+time java -Xmx8g -jar $GATK/GenomeAnalysisTK.jar \
+-R $DB \
 -T VariantFiltration \
 -o snps.filter.vcf \
 --variant snps.raw.vcf \
@@ -331,9 +361,9 @@ time java -Xmx8g -jar /Users/yvans/Home/bin/GenomeAnalysisTK-1.4-37-g0b29d54/Gen
 
 echo
 echo "hard filtration INDELs"
-time java -Xmx8g -jar /Users/yvans/Home/bin/GenomeAnalysisTK-1.4-37-g0b29d54/GenomeAnalysisTK.jar \
+time java -Xmx8g -jar $GATK/GenomeAnalysisTK.jar \
 -T VariantFiltration \
--R /Users/yvans/Home/bin/GATK_resource_bundle_from_Ying_17_01_2012/1.2/b37/bwa_v5.10/human_g1k_v37_decoy.fasta \
+-R $DB \
 -o indel.filter.vcf \
 --variant indel.raw.vcf \
 --filterExpression "QD < 2.0"  \
@@ -344,12 +374,12 @@ time java -Xmx8g -jar /Users/yvans/Home/bin/GenomeAnalysisTK-1.4-37-g0b29d54/Gen
 
 echo
 echo "GATK eval"
-time java -Xmx8g -jar /Users/yvans/Home/bin/GenomeAnalysisTK-1.4-37-g0b29d54/GenomeAnalysisTK.jar \
--R /Users/yvans/Home/bin/GATK_resource_bundle_from_Ying_17_01_2012/1.2/b37/bwa_v5.10/human_g1k_v37_decoy.fasta \
+time java -Xmx8g -jar $GATK/GenomeAnalysisTK.jar \
+-R $DB \
 -T VariantEval -o output.eval.gatkreport \
 --eval:set1 snps.filter.vcf \
---dbsnp /Users/yvans/Home/bin/GATK_resource_bundle_from_Ying_17_01_2012/1.2/b37/dbsnp_132.b37.vcf 2>errVarinantEval \
-> variantEval.txt
+--dbsnp /Users/yvans/Home/bin/GATK_resource_bundle_from_Ying_17_01_2012/1.2/b37/dbsnp_132.b37.vcf \
+2>errVarinantEval > variantEval.txt
 
 echo
 echo "annovar"
