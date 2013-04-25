6f702bd2f4c747f012a465a5eb8f7c1cf9f8c952
yvancouver <yvan.strahm@gmail.com>
2013-04-25 08:36:06 +0200

    m

--- reFormathsMetrics.sh
+++ reFormathsMetrics.sh
@@ -1,20 +1,18 @@
-cat $1| awk -F "\t" ' { 													### NF number of field
-if ( NF != 0 ) 										### RS 
-{													### FS Filed Separator
-if ( $1 ~/^#/) 										### test if line start with #
-{
-print $0
-}
-else												### 
-for (f = 1; f <= NF; f++)					
-a[NR, f] = $f										### NR
+cat $1| awk -F "\t" ' {
+if ( NF != 0 ){
+	FS Filed Separator
+		if ( $1 ~/^#/) {
+			print $0}
+		else
+			for (f = 1; f <= NF; f++)
+				a[NR, f] = $f
 }
 else 
-print $0
+	print $0
 }
 NF > nf { nf = NF }
 END {
-for (f = 1; f <= nf; f++)
-for (r = 1; r <= NR; r++)
-printf a[r, f] (r==NR ? RS : FS) 
+	for (f = 1; f <= nf; f++)
+	for (r = 1; r <= NR; r++)
+		printf a[r, f] (r==NR ? RS : FS) 
 }' | awk '{sub(/\t{6}/,"\t");print}'
